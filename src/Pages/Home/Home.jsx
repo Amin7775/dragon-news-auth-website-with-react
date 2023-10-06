a02a9f1648e697a6d4490a987baac75d3fe131cf
Amin7775 <amin.dpi24@gmail.com>
2023-10-06 23:31:37 +0600

    custom tailwind font setup

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            <div className="text-2xl">THIS IS HOME</div>
+            <div className="text-2xl font-poppins">THIS IS HOME</div>
         </div>
     );
 };
