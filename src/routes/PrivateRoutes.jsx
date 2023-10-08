6ed2f2d66f4a4cdd8ed10beb555b7e230e88a5e2
Amin7775 <amin.dpi24@gmail.com>
2023-10-09 00:46:10 +0600

    Auth redirect after login and Environment variables

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../provider/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 
 const PrivateRoutes = ({children}) => {
     const {user,loading}= useContext(AuthContext);
+
+    const location = useLocation();
+    console.log(location)
 
     if(loading){
         return <span className="loading loading-infinity loading-lg"></span>
@@ -14,7 +17,7 @@ const PrivateRoutes = ({children}) => {
         return children;
     }
 
-    return <Navigate to={'/login'}></Navigate>;
+    return <Navigate state={location.pathname} to={'/login'}></Navigate>;
 };
 
 export default PrivateRoutes;
